3134fa4aee0484d751a7709934551ea2b2e80e21
PenPen812 <penelope.c.leung@gmail.com>
2022-10-09 10:12:39 -0500

    modified JS line 1-5

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,22 +1,20 @@
+var weatherContainerEl = document.querySelector('#weather-container')
+var historyCardEl = document.querySelector('#historyCard')
+var forecastEl = document.querySelector('#forecast')
+var weatherCardEl = document.querySelector('#weatherCard')
+var cityNameEl = document.querySelector('#cityName')
+
 var APIKey = '50fab96ea22aedb450aa902a45727be1'
 var city;
-var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
-
-
-
-
-
 
 var currentWeatherEl = $('#currentWeather');
     function currentResult(){
         currentResult.text(queryURL);
         var currentWeather=document.createElement('div');
-        var cityName=document.createElement('h2');
         var temp=document.createElement('p');
         var wind=document.createElement('p');
         var humidity=document.createElement('p');
         currentWeather.setAttribute('class','currentWeather');
-        cityName=setAttribute('class','cityName');
         temp=setAttribute('class','temp');
         wind=setAttribute('class','wind');
         humidity=setAttribute('class','humidity');
@@ -44,7 +42,7 @@ for (let i=0; i<localStorage.length; i++){
 alert(`${city}: ${localStorage.getItem("city")}`);
 }
 
-var historyCard=document.getElementById('history')
+var historyCard=document.getElementById('historyCard')
 
 
 //Presented with city name, date, icon of weather, temperature, humidity, and wind speed
@@ -53,24 +51,26 @@ var historyCard=document.getElementById('history')
 //meta.lastupdate
 //meta.nextupdate
 //list.wind.speed
+var getApiData = function(city){
+  var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
 fetch(queryURL)
-.then(function (weather) {
-    if (weather.result) {
-      console.log(city,date,icon,temp,wind,humidity);
-      weather.json().then(function (data) {
-        console.log(data);
-        displayCity(city,date,icon,temp,wind,humidity);
-      });
- 
-    var row=document.createElement('div')
-    var hour=document.createElement('p')
-    var textarea=document.createElement('textarea')
-    var saveBtn=document.createElement('button')
+.then(function (response) {
+  if (response.result) {
+    console.log(city, date, icon, temp, wind, humidity);
+    response.json().then(function (data) {
+      console.log(data);
+      displayCityResult(city, date, icon, temp, wind, humidity);
+    });
+  } 
 
-    
-      
+  var displayCityResult = function (city) {
+    if  (city.length=0) {
+    weatherContainerEl.textContent = 'No City found.';
+      return;
+  }
+}
 //5-day forecast displays
 
 
-//Click on search history to show current and future forecast
+//Click on search history to show current and future forecast}
